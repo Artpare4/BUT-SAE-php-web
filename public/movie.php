14ee120845e84fefc6f353a70133f28034a94641
Erwan Lecomte <erwan.lecomte@etudiant.univ-reims.fr>
2023-06-12 15:07:22 +0200

    Structure d'exception
    Mise en place de la structure des exceptions de la page ainsi que de redirection en cas de mauvais paramètre.

--- public/movie.php
+++ public/movie.php
@@ -1,7 +1,25 @@
 <?php
+
 declare(strict_types=1);
 use Html\WebPage;
 use Entity\Actor;
 use Entity\Cast;
 use Entity\Movie;
+use Entity\Exception\EntityNotFoundException;
+use Exception\ParameterException;
 
+if (!isset($_GET['movieId']) or !ctype_digit($_GET['movieId'])) {
+    header("Location: index.php");
+} else {
+    $movieId = $_GET['movieId'];
+}
+
+try {
+
+} catch (ParameterException) {
+    http_response_code(400);
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+} catch (Exception) {
+    http_response_code(500);
+}
