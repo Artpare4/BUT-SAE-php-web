d6e9afc24147ded0c97991f86927d0513dcdb226
Erwan Lecomte <erwan.lecomte@etudiant.univ-reims.fr>
2023-06-12 10:04:55 +0200

    Création Actor
    Création de la classe, définition de ses getters et setters.

---
+++ src/Entity/Actor.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Entity;
+
+class Actor
+{
+    private int $avatarId;
+    private string $birthday;
+    private ?string $deathday;
+    private string $name;
+    private string $biography;
+    private string $placeOfBirth;
+
+    /**
+     * @return int
+     */
+    public function getAvatarId(): int
+    {
+        return $this->avatarId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBirthday(): string
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDeathday(): ?string
+    {
+        return $this->deathday;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBiography(): string
+    {
+        return $this->biography;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlaceOfBirth(): string
+    {
+        return $this->placeOfBirth;
+    }
+
+    /**
+     * @param int $avatarId
+     */
+    public function setAvatarId(int $avatarId): void
+    {
+        $this->avatarId = $avatarId;
+    }
+
+    /**
+     * @param string $birthday
+     */
+    public function setBirthday(string $birthday): void
+    {
+        $this->birthday = $birthday;
+    }
+
+    /**
+     * @param string|null $deathday
+     */
+    public function setDeathday(?string $deathday): void
+    {
+        $this->deathday = $deathday;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string $biography
+     */
+    public function setBiography(string $biography): void
+    {
+        $this->biography = $biography;
+    }
+
+    /**
+     * @param string $placeOfBirth
+     */
+    public function setPlaceOfBirth(string $placeOfBirth): void
+    {
+        $this->placeOfBirth = $placeOfBirth;
+    }
+
+
+}
