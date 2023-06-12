93a92f0dc96645c2836708f2408ba0465a1b6d0f
Erwan Lecomte <erwan.lecomte@etudiant.univ-reims.fr>
2023-06-12 18:07:54 +0200

    Ajustement de l'affichage des Acteurs sur mobile
    Changement css et HTML afin de rendre l'affichage cohérent dans une optique de MobileFirst sur les acteurs

--- public/movie.php
+++ public/movie.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 use Html\WebPage;
 use Entity\Actor;
 use Entity\Collection\CastCollection;
-use Entity\Cast;
 use Entity\Movie;
 use Entity\Exception\EntityNotFoundException;
 use Exception\ParameterException;
@@ -22,7 +21,7 @@ try {
     $html->appendCssUrl("css/movie.css");
     $html->setTitle($myMovie->getTitle());
     $html->appendContent("<header><h1>{$myMovie->getTitle()}</h1></header>\n");
-    $html->appendContent("<main><content class='movie'><div class='imgContent'><img src='img/movie.png'></div>");
+    $html->appendContent("<main><content class='movie'><div class='imgContent'><img src='img/movie.png' alt=''></div>");
 
     $html->appendContent("<div class='infoContent'><div class='titleDate'><div class='title'>{$myMovie->getTitle()}</div>");
     $html->appendContent("<div class='date'>{$myMovie->getReleaseDate()}</div></div>");
@@ -34,7 +33,7 @@ try {
     $casts = CastCollection::findByMovieId($movieId);
     foreach ($casts as $cast) {
         $actor = Actor::findById($cast->getActorId());
-        $html->appendContent("<content class='actor'><div class='imgContent'><img src='img/movie.png'></div>");
+        $html->appendContent("<content class='actor'><div class='imgContent'><img src='img/movie.png' alt=''></div>");
         $html->appendContent("<div class='infoContent'><div class='actorInfo'>{$cast->getRole()}</div>");
         $html->appendContent("<div class='actorInfo'>{$actor->getName()}</div></div></content>");
     }
