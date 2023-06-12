556dd5ca7ffa76a1291b43b285f87a6bf3530ca7
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-12 16:28:06 +0200

    Ajout de tests sur le vérification de $_Get dans image.php

--- public/image.php
+++ public/image.php
@@ -1,4 +1,13 @@
 <?php
-try{
-    
-}catch ();
+
+use Entity\Exception\ParameterException;
+use Entity\Image;
+try {
+    if(!isset($_GET['imageId'])|| !ctype_digit($_GET['imageId'])) {
+        throw new ParameterException();
+    }
+    $image=new Image();
+
+} catch (ParameterException) {
+
+}
