a737d328de0fa2b4139fdd34b10cf0862d135eb5
Erwan Lecomte <erwan.lecomte@etudiant.univ-reims.fr>
2023-06-12 17:51:14 +0200

    Ajustement de l'affichage du Film sur mobile
    Changement css et HTML afin de rendre l'affichage cohérent dans une optique de MobileFirst

--- public/movie.php
+++ public/movie.php
@@ -19,24 +19,27 @@ if (!isset($_GET['movieId']) or !ctype_digit($_GET['movieId'])) {
 try {
     $myMovie = Movie::findById($movieId);
     $html = new WebPage();
+    $html->appendCssUrl("css/movie.css");
     $html->setTitle($myMovie->getTitle());
-    $html->appendContent("<header><h1>{$myMovie->getTitle()}</h1>\n");
+    $html->appendContent("<header><h1>{$myMovie->getTitle()}</h1></header>\n");
     $html->appendContent("<main><content class='movie'><div class='imgContent'><img src='img/movie.png'></div>");
 
     $html->appendContent("<div class='infoContent'><div class='titleDate'><div class='title'>{$myMovie->getTitle()}</div>");
     $html->appendContent("<div class='date'>{$myMovie->getReleaseDate()}</div></div>");
 
-    $html->appendContent("<div class='originalTitle'>{$myMovie->getOriginalTitle()}</div>");
-    $html->appendContent("<div class='tagline'>{$myMovie->getTagline()}</div>");
-    $html->appendContent("<div class='overview'>{$myMovie->getOverview()}</div>");
+    $html->appendContent("<div class='info'>{$myMovie->getOriginalTitle()}</div>");
+    $html->appendContent("<div class='info'>{$myMovie->getTagline()}</div>");
+    $html->appendContent("<div class='info'>{$myMovie->getOverview()}</div></div></content>");
 
     $casts = CastCollection::findByMovieId($movieId);
     foreach ($casts as $cast) {
         $actor = Actor::findById($cast->getActorId());
         $html->appendContent("<content class='actor'><div class='imgContent'><img src='img/movie.png'></div>");
         $html->appendContent("<div class='infoContent'><div class='actorInfo'>{$cast->getRole()}</div>");
-        $html->appendContent("<div class='actorInfo'>{$actor->getName()}</div></div>");
+        $html->appendContent("<div class='actorInfo'>{$actor->getName()}</div></div></content>");
     }
+
+    $html->appendContent("</main>");
     echo $html->toHTML();
 } catch (ParameterException) {
     http_response_code(400);
