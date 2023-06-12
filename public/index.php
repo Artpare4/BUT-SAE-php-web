b3c0942902002ee58982b05444bab4bc937e7c8e
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-12 14:02:15 +0200

    Ajout de la structure principale de la page index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,21 @@
 <?php
+
 declare(strict_types=1);
 
-
+use Entity\Collection\MovieCollection;
 use Html\WebPage;
 
 $pageWeb=new WebPage();
+
+$pageWeb->setTitle("Films");
+
+$pageWeb->appendContent(<<<HTML
+    <div class="Header"><h1>Films</h1></div>
+HTML);
+
+$pageWeb->appendContent(<<<HTML
+    <div>{$pageWeb->getLastModification()}</div>
+HTML);
+
+
+echo $pageWeb->toHTML();
