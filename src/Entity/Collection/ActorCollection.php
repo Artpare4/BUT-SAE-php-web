1e30c126198d7945ca1e44b56a36801f0b2afc03
Erwan Lecomte <erwan.lecomte@etudiant.univ-reims.fr>
2023-06-12 11:05:37 +0200

    Ajout findAll dans ActorCollection
    Ajout de la méthode findAll() dans ActorCollection.

--- src/Entity/Collection/ActorCollection.php
+++ src/Entity/Collection/ActorCollection.php
@@ -8,5 +8,21 @@ use PDO;
 
 class ActorCollection
 {
+    /**
+     * Méthode de classe de ActorCollection
+     * Retourne l'entièreté des Acteurs présents dans la base de données sous forme d'un tableau indexé.
+     *
+     * @return Actor[]
+     */
+    public static function findAll(): array
+    {
+        $request = MyPdo::getInstance()->prepare(<<<SQL
+            SELECT id, avatarId, birthday, deathday, name, biography, placeOfBirth
+            FROM people
+            ORDER BY name;
+        SQL);
+        $request->execute();
+        return $request->fetchAll(PDO::FETCH_CLASS, Actor::class);
 
+    }
 }
