dc9f4de2b41a1edcd2eac52ffc615c55024b526b
Erwan Lecomte <erwan.lecomte@etudiant.univ-reims.fr>
2023-06-12 13:55:43 +0200

    Ajout méthode findByMovie dans CastCollection
    Ajout de la méthode findByMovie(), doc non écrite et Exception non gérée.

--- src/Entity/Collection/CastCollection.php
+++ src/Entity/Collection/CastCollection.php
@@ -1,11 +1,29 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Entity\Collection;
 
 use Database\MyPdo;
 use Entity\Cast;
 use PDO;
+
 class CastCollection
 {
+    public static function findByMovie(int $id): array
+    {
+        $request = MyPdo::getInstance()->prepare(<<<SQL
+            SELECT id, movieId, peopleId as "actorId", role, orderIndex
+            FROM cast
+            WHERE movieId = ?;
+        SQL);
+        $request->bindValue(1, $id);
+        $request->execute();
+        if ($results = $request->fetchAll(PDO::FETCH_CLASS, Cast::class)) {
+            return $results;
+        } else {
+            throw new Exception();
+        }
+    }
 
 }
