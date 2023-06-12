ba90bf64516b94bcd4272cfae3d62d7e8b9045a6
Erwan Lecomte <erwan.lecomte@etudiant.univ-reims.fr>
2023-06-12 16:43:41 +0200

    Ajout des Acteurs du Cast
    Ajout des acteurs dans la page HTML ainsi que l'architecture d'affichage de ces derniers.

--- public/movie.php
+++ public/movie.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 use Html\WebPage;
 use Entity\Actor;
+use Entity\Collection\CastCollection;
 use Entity\Cast;
 use Entity\Movie;
 use Entity\Exception\EntityNotFoundException;
@@ -12,7 +13,7 @@ if (!isset($_GET['movieId']) or !ctype_digit($_GET['movieId'])) {
     header("Location: index.php");
     exit();
 } else {
-    $movieId = $_GET['movieId'];
+    $movieId = intval($_GET['movieId']);
 }
 
 try {
@@ -20,7 +21,23 @@ try {
     $html = new WebPage();
     $html->setTitle($myMovie->getTitle());
     $html->appendContent("<header><h1>{$myMovie->getTitle()}</h1>\n");
+    $html->appendContent("<main><content class='movie'><div class='imgContent'><img src='img/movie.png'></div>");
 
+    $html->appendContent("<div class='infoContent'><div class='titleDate'><div class='title'>{$myMovie->getTitle()}</div>");
+    $html->appendContent("<div class='date'>{$myMovie->getReleaseDate()}</div></div>");
+
+    $html->appendContent("<div class='originalTitle'>{$myMovie->getOriginalTitle()}</div>");
+    $html->appendContent("<div class='tagline'>{$myMovie->getTagline()}</div>");
+    $html->appendContent("<div class='overview'>{$myMovie->getOverview()}</div>");
+
+    $casts = CastCollection::findByMovieId($movieId);
+    foreach ($casts as $cast) {
+        $actor = Actor::findById($cast->getActorId());
+        $html->appendContent("<content class='actor'><div class='imgContent'><img src='img/movie.png'></div>");
+        $html->appendContent("<div class='infoContent'><div class='actorInfo'>{$cast->getRole()}</div>");
+        $html->appendContent("<div class='actorInfo'>{$actor->getName()}</div></div>");
+    }
+    echo $html->toHTML();
 } catch (ParameterException) {
     http_response_code(400);
 } catch (EntityNotFoundException) {
