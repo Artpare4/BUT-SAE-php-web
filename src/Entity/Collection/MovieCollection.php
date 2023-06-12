4972079ad902bc7f566d57f602c96821451f6d86
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-12 11:23:48 +0200

    Ajout de la méthode findByType

--- src/Entity/Collection/MovieCollection.php
+++ src/Entity/Collection/MovieCollection.php
@@ -23,4 +23,25 @@ SQL);
         $res=$request->fetchAll(PDO::FETCH_CLASS, Movie::class);
         return $res;
     }
+
+    /**
+     * Méthode de la classe MovieCollection. Cette méthode retourne une liste d'entité Movie appartenant au genre passé en paramtre.
+     * @param string $idType id du genre
+     * @return Movie[] liste de film
+     */
+    public function getByType(string $idType): array
+    {
+        $res=[];
+        $request=MyPdo::getInstance()->prepare(<<<SQL
+        SELECT *
+        FROM movie m,
+             movie_genre mg
+        WHERE m.id=mg.movieId
+        AND mg.genreId=:idType;
+SQL);
+        $request->execute([':idType'=>$idType]);
+        $res=$request->fetchAll(PDO::FETCH_CLASS, Movie::class);
+        return $res;
+    }
 }
+
