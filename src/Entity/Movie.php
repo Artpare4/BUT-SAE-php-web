ec91d1c4f3d92271264b13c1c4280a498c89abf8
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-12 10:03:47 +0200

    Ajout des attributs de la classes Movie

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -4,5 +4,14 @@ namespace Entity;
 
 class Movie
 {
-    
+    private int $id;
+    private string $originalLanguage;
+    private string $originalTitle;
+    private string $overview;
+    private string $releaseDate;
+    private int $runtime;
+    private string $tagline;
+    private string $title;
+    private int $posterId;
+
 }
