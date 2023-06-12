4b8ab1b06ad1e96b7262a38e8390bfa7ed3db2e7
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-12 16:43:29 +0200

    Ajout de la gestion des exception dans image.php et de l'affichage du jpeg

--- public/image.php
+++ public/image.php
@@ -1,13 +1,19 @@
 <?php
 
 use Entity\Exception\ParameterException;
+use Entity\Exception\EntityNotFoundException;
 use Entity\Image;
+
 try {
     if(!isset($_GET['imageId'])|| !ctype_digit($_GET['imageId'])) {
         throw new ParameterException();
     }
     $image=new Image();
-
-} catch (ParameterException) {
-
+    $res=$image->getById($_GET['imageId']);
+    echo $res->getJpeg();
+} catch (EntityNotFoundException) {
+    echo "http://cutrona/but/s2/sae2-01/ressources/public/img/movie.png";
+}
+catch(ParameterException) {
+    http_response_code(400);
 }
