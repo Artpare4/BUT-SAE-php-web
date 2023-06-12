ae112b55cdafafb483c01cc4c07559d93f20ee0a
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-12 11:15:37 +0200

    Ajout de la méthode getAllMovie de la classe MovieCollection

--- src/Entity/Collection/MovieCollection.php
+++ src/Entity/Collection/MovieCollection.php
@@ -2,7 +2,25 @@
 
 namespace Entity\Collection;
 
+use PDO;
+use Entity\Movie;
+use Database\MyPdo;
+
 class MovieCollection
 {
-
+    /**
+     * Méthode de la classe MovieCollection. Cette méthode retourne une liste d'entité Movie
+     * @return Movie[] List d'entité movie
+     */
+    public function getAllMovie(): array
+    {
+        $res=[];
+        $request=MyPdo::getInstance()->prepare(<<<SQL
+        SELECT *
+        FROM movie;
+SQL);
+        $request->execute();
+        $res=$request->fetchAll(PDO::FETCH_CLASS, Movie::class);
+        return $res;
+    }
 }
