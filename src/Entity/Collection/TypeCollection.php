0ab1e7e9966f5e458efde963e7b458394247dc94
Erwan Lecomte <erwan.lecomte@etudiant.univ-reims.fr>
2023-06-12 11:26:03 +0200

    Ajout findAll() dans TypeCollection

--- src/Entity/Collection/TypeCollection.php
+++ src/Entity/Collection/TypeCollection.php
@@ -8,5 +8,20 @@ use Database\MyPdo;
 use Entity\Type;
 class TypeCollection
 {
-
+    /**
+     * Méthode de classe de TypeCollection
+     * Retourne l'entièreté des Genres présents dans la base de données sous forme d'un tableau indexé.
+     *
+     * @return Type[]
+     */
+    public static function findAll(): array
+    {
+        $request = MyPdo::getInstance()->prepare(<<<SQL
+            SELECT id, name
+            FROM genre
+            ORDER BY name;
+        SQL);
+        $request->execute();
+        return $request->fetchAll(PDO::FETCH_CLASS, Type::class);
+    }
 }
