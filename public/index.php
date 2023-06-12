57d03505453f68470fd6698aa35417963a95d017
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-12 14:10:26 +0200

    Ajout de l'affichage des films sur la page Web dans index.php

--- public/index.php
+++ public/index.php
@@ -10,8 +10,22 @@ $pageWeb=new WebPage();
 $pageWeb->setTitle("Films");
 
 $pageWeb->appendContent(<<<HTML
-    <div class="Header"><h1>Films</h1></div>
+    <div class='Header'><h1>Films</h1></div>
 HTML);
+
+
+$filmCollection=new MovieCollection();
+$Collection=$filmCollection->getAllMovie();
+$pageWeb->appendContent("<div class='main'>");
+
+foreach ($Collection as $film) {
+    $pageWeb->appendContent(<<<HTML
+    <div class="movie"><img src=""><p>{$film->getTitle()}</p></div>
+HTML);
+}
+
+
+
 
 $pageWeb->appendContent(<<<HTML
     <div>{$pageWeb->getLastModification()}</div>
