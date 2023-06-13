f3f5d69209971144d2679f677855bc8d232f012b
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-13 09:27:14 +0200

    Ajout de la gestion des erreurs pour les photos d'acteur manquant

--- public/imageActor.php
+++ public/imageActor.php
@@ -1,1 +1,17 @@
 <?php
+use Entity\Exception\ParameterException;
+use Entity\Exception\EntityNotFoundException;
+use Entity\Image;
+
+try {
+    if(!isset($_GET['imageId'])|| !ctype_digit($_GET['imageId'])) {
+        throw new EntityNotFoundException();
+    }
+    $image=new Image();
+    $res=$image->getById($_GET['imageId']);
+    header('HTTP 1.1 200 OK');
+    header('Content-Type: image/jpeg');
+    echo $res->getJpeg();
+} catch (EntityNotFoundException) {
+    header('Location: /image/actor.png');
+}
