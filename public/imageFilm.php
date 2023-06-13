57caf5e8663a94ec7028b0765d25944f5f8c850b
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-13 09:22:43 +0200

    Renommage du fichier image.php et imageFilm.php

--- public/imageFilm.php
+++ public/imageFilm.php
@@ -14,8 +14,7 @@ try {
     header('Content-Type: image/jpeg');
     echo $res->getJpeg();
 } catch (EntityNotFoundException) {
-    header('HTTP 1.1 200 OK');
-    header('Location: http://cutrona/but/s2/sae2-01/ressources/public/img/movie.png');
+    header('Location: /image/default_picture.png');
 } catch(ParameterException) {
     http_response_code(400);
 }
