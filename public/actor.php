8c358455de6bddf9e1b0c54f45bfaf98bc900f54
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-13 16:35:40 +0200

    Ajout de la protection des caractères spéciaux avec escapestring dans le fichier actor.php

--- public/actor.php
+++ public/actor.php
@@ -16,23 +16,23 @@ try {
     $myActor = Actor::findById($actorId);
     $html = new WebPage();
     $html->appendCssUrl("css/content.css");
-    $html->setTitle($myActor->getName());
-    $html->appendContent("<header><h1>Acteur - {$myActor->getName()}</h1></header>\n");
+    $html->setTitle($html->escapeString($myActor->getName()));
+    $html->appendContent("<header><h1>Acteur - {$html->escapeString($myActor->getName())}</h1></header>\n");
     $html->appendContent("<main><content class='principal'><div class='imgContent'><img class='mainImg' src='imageActor.php?imageId=".$myActor->getAvatarId()."' alt='Image Acteur'></div>");
 
-    $html->appendContent("<div class='infoContent'><div class='info'>{$myActor->getName()}</div>");
-    $html->appendContent("<div class='info'>{$myActor->getPlaceOfBirth()}</div>");
-    $birthDate = ($myActor->getBirthday()) ?: "Naissance inconnue";
+    $html->appendContent("<div class='infoContent'><div class='info'>{$html->escapeString($myActor->getName())}</div>");
+    $html->appendContent("<div class='info'>{$html->escapeString($myActor->getPlaceOfBirth())}</div>");
+    $birthDate = ($html->escapeString($myActor->getBirthday())) ?: "Naissance inconnue";
     $deathDate = ($myActor->getDeathday()) ?: "Mort inconnue / En vie";
-    $html->appendContent("<div class='info'><div class='dates'>$birthDate</div> - <div class='dates'>$deathDate</div></div>");
-    $html->appendContent("<div class='info'>{$myActor->getBiography()}</div></div></content>");
+    $html->appendContent("<div class='info'><div class='dates'>$birthDate</div> - <div class='dates'>{$html->escapeString($deathDate)}</div></div>");
+    $html->appendContent("<div class='info'>{$html->escapeString($myActor->getBiography())}</div></div></content>");
 
     $casts = CastCollection::findByActorId($actorId);
     foreach ($casts as $cast) {
         $movie = Movie::findById($cast->getMovieId());
         $html->appendContent("<content class='secondary'><a class ='secondary' href='movie.php?movieId=".$cast->getMovieId()."'><div class='imgContent'><img src='imageFilm.php?imageId=".$movie->getPosterId()."' alt='Image Film'></div>");
-        $html->appendContent("<div class='infoContent'><div class='secondaryInfo'><div class='titleDate'><div class='title'>{$movie->getTitle()}</div><div class='date'>{$movie->getReleaseDate()}</div></div></div>");
-        $html->appendContent("<div class='secondaryInfo'>{$cast->getRole()}</div></div></div></a></content>");
+        $html->appendContent("<div class='infoContent'><div class='secondaryInfo'><div class='titleDate'><div class='title'>{$html->escapeString($movie->getTitle())}</div><div class='date'>{$html->escapeString($movie->getReleaseDate())}</div></div></div>");
+        $html->appendContent("<div class='secondaryInfo'>{$html->escapeString($cast->getRole())}</div></div></div></a></content>");
     }
 
     $html->appendContent("</main>");
