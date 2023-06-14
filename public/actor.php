8010eb44191fc3ad5b1a8c7a75f0c7770aa9502b
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-14 14:00:57 +0200

    Changement du nom de la classe des balises de la page actor

--- public/actor.php
+++ public/actor.php
@@ -20,20 +20,20 @@ try {
     $html->appendContent("<header><h1>Acteur - {$html->escapeString($myActor->getName())}</h1></header>\n");
     $html->appendContent("<main><content class='principal'><div class='imgContent'><img class='mainImg' src='imageActor.php?imageId=".$myActor->getAvatarId()."' alt='Image Acteur'></div>");
 
-    $html->appendContent("<div class='infoContent'><div class='info'>{$html->escapeString($myActor->getName())}</div>");
-    $html->appendContent("<div class='info'>{$html->escapeString($myActor->getPlaceOfBirth())}</div>");
+    $html->appendContent("<div class='infoContent'><div class='info nomacteur'>{$html->escapeString($myActor->getName())}</div>");
+    $html->appendContent("<div class='info lieuNaissance'>{$html->escapeString($myActor->getPlaceOfBirth())}</div>");
     $birthDate = ($myActor->getBirthday()) ?: "Naissance inconnue";
     $birthDate = $html->escapeString($birthDate);
     $deathDate = ($myActor->getDeathday()) ?: "Mort inconnue / En vie";
     $deathDate = ($html->escapeString($deathDate));
-    $html->appendContent("<div class='info'><div class='dates'>$birthDate</div> - <div class='dates'>{$html->escapeString($deathDate)}</div></div>");
-    $html->appendContent("<div class='info'>{$html->escapeString($myActor->getBiography())}</div></div></content>");
+    $html->appendContent("<div class='info'><div class='dates birth'>$birthDate</div> - <div class='dates death'>{$html->escapeString($deathDate)}</div></div>");
+    $html->appendContent("<div class='info Biographie'>{$html->escapeString($myActor->getBiography())}</div></div></content>");
 
     $casts = CastCollection::findByActorId($actorId);
     foreach ($casts as $cast) {
         $movie = Movie::findById($cast->getMovieId());
         $html->appendContent("<content class='secondary'><a class ='secondary' href='movie.php?movieId=".$cast->getMovieId()."'><div class='imgContent'><img src='imageFilm.php?imageId=".$movie->getPosterId()."' alt='Image Film'></div>");
-        $html->appendContent("<div class='infoContent'><div class='secondaryInfo'><div class='titleDate'><div class='title'>{$html->escapeString($movie->getTitle())}</div><div class='date'>{$html->escapeString($movie->getReleaseDate())}</div></div></div>");
+        $html->appendContent("<div class='infoContent'><div class='secondaryInfo'><div class='titleDate'><div class='titleMovieDesc'>{$html->escapeString($movie->getTitle())}</div><div class='date'>{$html->escapeString($movie->getReleaseDate())}</div></div></div>");
         $html->appendContent("<div class='secondaryInfo'>{$html->escapeString($cast->getRole())}</div></div></div></a></content>");
     }
 
