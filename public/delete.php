ec520dfca782c471b0e613ca64ca9ce867e0b777
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-14 10:59:43 +0200

    Ajout des tests et de la fonctionnalité de suppression du film

--- public/delete.php
+++ public/delete.php
@@ -1,3 +1,12 @@
 <?php
 
 declare(strict_types=1);
+use Entity\Exception\ParameterException;
+use Entity\Movie;
+
+if(!isset($_GET['idmovie'])||!ctype_digit($_GET['idmovie'])) {
+    throw new ParameterException();
+}
+$moviedelete=new Movie();
+$moviedelete->setId($_GET['idmovie']);
+$moviedelete->delete();
