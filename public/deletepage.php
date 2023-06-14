e1219611b477f4111702d151af57c68f4173c70f
Parent Arthur <arthur.parent@etudiant.univ-reims.fr>
2023-06-14 10:51:42 +0200

    Supperssion contenu de deletepage.php

--- public/deletepage.php
+++ public/deletepage.php
@@ -1,11 +1,3 @@
 <?php
-declare(strict_types=1);
-use Html\WebPage;
-use Html\Form\MovieForm;
-$pageDelete=new WebPage();
-$form=new MovieForm();
-$pageDelete->setTitle("Page de suppression");
 
-$pageDelete->appendContent(<<<HTML
-    <header>Page du suppression du film </header>
-HTML);
+declare(strict_types=1);
