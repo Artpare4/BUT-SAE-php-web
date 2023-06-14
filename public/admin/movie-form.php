86b6045b837f4ae6f0bc18d6bd7ddd57538e7058
Erwan Lecomte <erwan.lecomte@etudiant.univ-reims.fr>
2023-06-14 13:55:55 +0200

    Mise en page du formulaire
    Mise en page pour rendre le formulaire compréhensible.

--- public/admin/movie-form.php
+++ public/admin/movie-form.php
@@ -12,6 +12,7 @@ try {
     if (!isset($_GET['movieId'])) {
         $MovieId=null;
         $form = new MovieForm();
+        $info = "Création d'un film";
     } else {
         if (!ctype_digit($_GET['movieId'])) {
             throw new ParameterException();
@@ -19,11 +20,23 @@ try {
             $movieId = intval($_GET['movieId']);
             $movie = Movie::findById($movieId);
             $form = new MovieForm($movie);
+            $info = "Modification du film \"{$movie->getTitle()}\"";
         }
     }
     $page = new WebPage();
     $page->setTitle('Modification des films');
-    $page->appendContent($form->getHtmlForm('movie-save.php'));
+    $page->appendContent("<header><h1>$info</h1></header>");
+
+    $page->appendCssUrl('/css/style.css');
+
+    $page->appendContent(<<<HTML
+    <main>
+        <content class="form">
+            {$form->getHtmlForm('movie-save.php')}
+        </content>
+    </main>
+    <footer>{$page->getLastModification()}</footer>
+    HTML);
     echo $page->toHTML();
 } catch (ParameterException) {
     http_response_code(400);
