d3caad57ed45c4ded0efb518e4cc78eaecb576a7
Erwan Lecomte <erwan.lecomte@etudiant.univ-reims.fr>
2023-06-14 14:07:38 +0200

    Changement de imageActor.php
    Le manque de paramètre imageId renvoie maintenant l'image par défault pour les acteurs.

--- public/imageActor.php
+++ public/imageActor.php
@@ -1,17 +1,17 @@
 <?php
-use Entity\Exception\ParameterException;
+use Exception\ParameterException;
 use Entity\Exception\EntityNotFoundException;
 use Entity\Image;
 
 try {
-    if(!isset($_GET['imageId'])|| !ctype_digit($_GET['imageId'])) {
-        throw new EntityNotFoundException();
+    if(!isset($_GET['imageId']) || !ctype_digit($_GET['imageId'])) {
+        throw new ParameterException();
     }
     $image=new Image();
     $res=$image->getById($_GET['imageId']);
     header('HTTP 1.1 200 OK');
     header('Content-Type: image/jpeg');
     echo $res->getJpeg();
-} catch (EntityNotFoundException) {
+} catch (EntityNotFoundException|ParameterException) {
     header('Location: /image/actor.png');
 }
